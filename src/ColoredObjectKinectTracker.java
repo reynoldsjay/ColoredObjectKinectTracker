cfb489ec7e3fe4caf83a32ca034ffcde184ce15a
reynoldsjay <jayrey000@gmail.com>
2015-02-24 22:26:38 -0500

    Initial commit

---
+++ src/ColoredObjectKinectTracker.java
@@ -0,0 +1,14 @@
+import processing.core.PApplet;
+
+/**
+ * Processing sketch to track a colored object in 3D with Kinect
+ * @author Jay Reynolds
+ */
+public class ColoredObjectKinectTracker extends PApplet {
+    public void setup() {
+
+
+    }
+    public void draw() {
+    }
+}
